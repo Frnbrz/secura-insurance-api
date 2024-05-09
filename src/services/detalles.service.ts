da0a3b04790821b0bba5f00e8e8377a4af9cd0b9
frnbrz <frnbrz@gmail.com>
2024-05-09 10:04:31 +0200

    chore: Update detalles.service.ts to include updateDetalleUno function

--- src/services/detalles.service.ts
+++ src/services/detalles.service.ts
@@ -1,6 +1,6 @@
 import detalle1Data from '../bd/detalle1.json'
 import detalle2Data from '../bd/detalle2.json'
-import { Detalle } from '../types/types'
+import { Detalle, OwnerData } from '../types/types'
 
 const detalle1: Detalle = detalle1Data as Detalle
 const detalle2: Detalle = detalle2Data as Detalle
@@ -21,4 +21,28 @@ async function getDetalleDos(): Promise<Detalle> {
   }
 }
 
-export { getDetalleDos, getDetalleUno }
+async function updateDetalleUno(
+  _id: string,
+  body: OwnerData
+): Promise<Detalle> {
+  try {
+    if (!body) {
+      throw new Error('Clientes no encontrado')
+    }
+
+    detalle1.ownerData.cif = body.cif || detalle1.ownerData.cif
+    detalle1.ownerData.name = body.name || detalle1.ownerData.name
+    detalle1.ownerData.surname = body.surname || detalle1.ownerData.surname
+    detalle1.ownerData.phone = body.phone || detalle1.ownerData.phone
+    detalle1.ownerData.email = body.email || detalle1.ownerData.email
+    detalle1.ownerData.fiscalAddress =
+      body.fiscalAddress || detalle1.ownerData.fiscalAddress
+    detalle1.ownerData.state = body.state || detalle1.ownerData.state
+
+    return detalle1
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
+}
+
+export { getDetalleDos, getDetalleUno, updateDetalleUno }
