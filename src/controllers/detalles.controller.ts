e96ba755684752a9d3f886af63dfe6348fb1db23
frnbrz <frnbrz@gmail.com>
2024-05-09 10:04:24 +0200

    feat: Add updateDetalle1 endpoint to detalles controller

--- src/controllers/detalles.controller.ts
+++ src/controllers/detalles.controller.ts
@@ -1,4 +1,8 @@
-import { getDetalleDos, getDetalleUno } from '../services/detalles.service'
+import {
+  getDetalleDos,
+  getDetalleUno,
+  updateDetalleUno
+} from '../services/detalles.service'
 import { StatusType } from '../types/enums'
 
 export const fetchDetalle1 = async (_req: any, res: any) => {
@@ -8,5 +12,12 @@ export const fetchDetalle1 = async (_req: any, res: any) => {
 
 export const fetchDetalle2 = async (_req: any, res: any) => {
   const clientes = await getDetalleDos()
+  res.send({ status: StatusType.OK, data: clientes })
+}
+
+export const updateDetalle1 = async (req: any, res: any) => {
+  const { id } = req.params
+  const { body } = req
+  const clientes = await updateDetalleUno(id, body)
   res.send({ status: StatusType.OK, data: clientes })
 }
