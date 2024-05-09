f1c5f8025659bc26e3fe9a643d7aaf893f33ae58
frnbrz <frnbrz@gmail.com>
2024-05-09 10:04:28 +0200

    feat: Add updateDetalle1 endpoint to detalles router

--- src/routes/detalles.router.ts
+++ src/routes/detalles.router.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import {
   fetchDetalle1,
-  fetchDetalle2
+  fetchDetalle2,
+  updateDetalle1
 } from '../controllers/detalles.controller'
 
 export const detalleRouter = express.Router()
 
 detalleRouter.get('/1', fetchDetalle1)
 detalleRouter.get('/2', fetchDetalle2)
+detalleRouter.patch('/1/ownerdata', updateDetalle1)
